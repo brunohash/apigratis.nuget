bfefe46a9d97d0ac77079f139932d36befff6452
brunohashimoto <bgmuniz@live.com>
2023-07-08 15:26:11 -0400

    feat: criação de testes unitários

---
+++ Tests/GenericCallerTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+using FluentAssertions;
+using ApiBrasil;
+using ApiBrasil.Domain;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class GenericCallerTests
+    {
+        [Fact]
+        public async Task Call_ShouldReturnContent()
+        {
+            // Arrange
+            var type = "exampleType";
+            var action = "exampleAction";
+            var content = "exampleContent";
+            var config = new ApiBrasilConfiguration
+            {
+                SecretKey = "exampleSecretKey",
+                PublicToken = "examplePublicToken",
+                DeviceToken = "exampleDeviceToken",
+                Authorization = "exampleAuthorization"
+            };
+
+            // Act
+            var result = await GenericCaller.Call(type, action, content, config);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<string>();
+            result.Should().NotBeEmpty();
+        }
+    }
+}
