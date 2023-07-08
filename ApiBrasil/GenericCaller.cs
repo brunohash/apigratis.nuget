93d256502f271860de37f722b92079f811a43a5c
brunohashimoto <bgmuniz@live.com>
2023-07-08 11:16:30 -0400

    fix: refatorando código

--- ApiBrasil/GenericCaller.cs
+++ ApiBrasil/GenericCaller.cs
@@ -2,6 +2,7 @@
 using ApiBrasil.Domain;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 
 namespace ApiBrasil
 {
@@ -14,7 +15,7 @@ namespace ApiBrasil
             _config = config.Value;
         }
 
-        public async Task<string> Request(string type, string action)
+        public async Task<string> SaveAsync(string type, string action)
         {
             var options = new RestClientOptions("https://cluster-01.apigratis.com")
             {
@@ -23,15 +24,15 @@ namespace ApiBrasil
             var client = new RestClient(options);
             var request = new RestRequest($"/api/v1/{type}/{action}", Method.Post);
             request.AddHeader("Content-Type", "application/json");
-            request.AddHeader("SecretKey", _config.SecretKey);
-            request.AddHeader("PublicToken", _config.PublicToken);
-            request.AddHeader("DeviceToken", _config.DeviceToken);
+            request.AddHeader("SecretKey", _config.SecretKey ?? "");
+            request.AddHeader("PublicToken", _config.PublicToken ?? "");
+            request.AddHeader("DeviceToken", _config.DeviceToken ?? "");
             request.AddHeader("Authorization", $"Bearer {_config.Authorization}");
             var body = "";
             request.AddStringBody(body, DataFormat.Json);
             RestResponse response = await client.ExecuteAsync(request);
 
-            return response.Content;
+            return JsonSerializer.Serialize(response.Content);
         }
     }
 
