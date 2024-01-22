807352b9a7d9bdc3642600a51328f22de0461d87
brunohashimoto <bgmuniz@live.com>
2024-01-22 19:40:12 -0400

    fix new version

--- ApiBrasil/GenericCaller.cs
+++ ApiBrasil/GenericCaller.cs
@@ -3,9 +3,9 @@ using ApiBrasil.Domain;
 
 namespace ApiBrasil
 {
-    public static class GenericCaller
+    public static class ApiBrasilCommunity
     {
-        public static async Task<string> Call(string type, string action, object content, ApiBrasilConfiguration config)
+        public static async Task<string> ApiBrasilCaller(string type, string action, object content, ApiBrasilConfiguration config)
         {
             var options = new RestClientOptions("https://gateway.apibrasil.io")
             {
